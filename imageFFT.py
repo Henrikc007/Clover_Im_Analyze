f6819a0f04d0815b45e7b8e5bce136313cb5c7ec
Henrikc007 <128123648+Henrikc007@users.noreply.github.com>
2024-11-05 11:40:09 +0100

    lavet en klasse som greenifyer og cropper

--- imageFFT.py
+++ imageFFT.py
@@ -2,10 +2,27 @@
 
 import numpy as np
 from PIL import Image
+import trimmer
 
-image = Image.open('IMG_1208.jpg')
-print(image.format)
-print(image.size)
-print(image.mode)
+mitbillede = trimmer.clovercandidate('IMG_1208.jpg')
+print(mitbillede.billede.format)
+print(mitbillede.billede.size)
+print(mitbillede.billede.mode)
 
-image.show()
+
+#image.show()
+cropimage=mitbillede.klipbillede(2100,1400)
+print(cropimage.size)
+
+
+fadeGreened=cropimage
+
+fadeGreened=trimmer.greenify(fadeGreened,180)
+fadeGreened=trimmer.greenify(fadeGreened,180)
+fadeGreened=trimmer.greenify(fadeGreened,180)
+fadeGreened.show()
+cropblackedge=trimmer.createBlackCopy(cropimage)[1]
+cropimage.show()
+
+
+
